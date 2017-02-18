2922f325e9e2173a9c3be7ac7cc398c3b2c1a74d
larisa4642 <larisa4642@yahoo.com>
2017-02-18 21:50:23 +0200

    error

---
+++ assignment3/app.js
@@ -0,0 +1,68 @@
+(function(){
+	'use strict';
+	angular.module('NarrowItDownApp',[])
+	.controller ('NarrowItDownController',NarrowItDownController)
+	.service('MenuSearchService',MenuSearchService)
+	.directive('foundItems', FoundItemsDirective )
+	.constant('ApiUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
+
+
+	function FoundItemsDirective() {
+	  var ddo = {
+	    scope: {
+	      foundItems: '<',
+	      onRemove: '&'
+	    }
+/*	    controller: NarrowItDownDirectiveController,
+	    controllerAs: 'narrowCtrl',
+	    bindToController: true*/
+	  };
+
+	  return ddo;
+	}
+
+/*	function NarrowItDownDirectiveController() {
+		var found=this;
+		console.log("directive controller"+found);
+	}*/
+
+	NarrowItDownController.$inject = ['MenuSearchService'];
+	function NarrowItDownController(MenuSearchService){
+		var narrowCtrl=this;
+		var promise=MenuSearchService.getMatchedMenuItems();
+
+		promise.then(function (response) {
+		    narrowCtrl.found = response;
+			console.log("found in ctrl"+narrowCtrl.found);
+
+		  })
+		  .catch(function (error) {
+		    console.log("Something went terribly wrong.");
+		  });	
+
+		narrowCtrl.removeItem = function () {
+	    console.log("functia din controller");
+	  	};
+	};
+	MenuSearchService.$inject = ['$http', 'ApiUrl'];
+	function MenuSearchService($http, ApiUrl){
+		var service = this;
+		service.getMatchedMenuItems=function(searchTerm){
+			var response= $http({
+					      method: "GET",
+					      url: ApiUrl
+					    })
+					.then(function (result) {
+						    // process result and only keep items that match
+						    var foundItems=result.data;
+                             console.log(JSON.stringify(result.data));
+						    // return processed items
+						    return foundItems;
+						});
+
+			return response;
+		};
+	};
+
+
+})();
